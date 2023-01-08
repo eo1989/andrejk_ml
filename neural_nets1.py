635a2ff2160a1225501cbe8612bcdc749d998c8d
ernest orlowski <eorlowski6@gmail.com>
2023-01-08 12:36:09 -0500

    trying to keep all these sync'd is a real bitch

---
+++ neural_nets1.py
@@ -0,0 +1,14 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:hydrogen,qmd,md
+#     text_representation:
+#       extension: .py
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Python(3.10.5)
+#     language: python
+#     name: python3105
+# ---
